79d0e97820a576dec4d3dd89ce8a22c3d8f2f2e5
jafrizzell <j.a.frizzell@hotmail.com>
2022-03-14 11:01:00 -0500

    Revert "create file tree structure"
    This reverts commit b4b81b3f4135743a290ab54132db64facce9e8eb.

---
+++ preprocess_code/preproc1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+path = "C:/Users/jafrizzell.22/Desktop/460Design/deep_sea_corals.csv"
+
+raw = pd.read_csv(path)
+
+
+def round_depth(x, base):
+    return int(base * round(float(x)/base))
+
+
+raw['depth'] = raw['depth'].apply(lambda x: round_depth(x, base=5))
+
+raw.to_csv('C:/Users/jafrizzell.22/Desktop/460Design/deep_sea_corals_rounded.csv')
---
+++ preprocess_code/preproc_rightjoin.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+coral = 'C:/Users/jafrizzell.22/Desktop/460Design/deep_sea_corals_rounded.csv'
+temps = 'C:/Users/jafrizzell.22/Desktop/460Design/woa18_decav_t00mn04.csv'
+
