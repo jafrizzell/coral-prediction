6d9b758cefcc952e46150d88dc1859a4cbbfdea2
jafrizzell <j.a.frizzell@hotmail.com>
2022-03-21 13:29:16 -0500

    worked on project

--- ml_code/MAIN.py
+++ ml_code/MAIN.py
@@ -6,8 +6,8 @@ modelpath = str(pathlib.Path(os.getcwd()).parent) + '/models/trial0.3.h5'
 
 CoralPredictor = CoralClass.CoralPrediction()
 CoralPredictor.set_model(modelpath)
-conditions = [38, -150, 1000, None, 20, None]
+conditions = [38, -150, 4000, None, 20, None]
 mean_pred, med_pred = CoralPredictor.predict(conditions)
-print('----------------------------------------')
+print('\n----------------------------------------')
 print('The average likelihood of coral growth is: ', mean_pred*100, '%')
 print('The median likelihood of coral growth is: ', med_pred*100, '%')
