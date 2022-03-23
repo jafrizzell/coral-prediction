5854d75c9f61f807d03421de26f4ad03724f594c
jafrizzell <j.a.frizzell@hotmail.com>
2022-03-23 11:18:16 -0500

    implemented command line runtine functionality

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # coral-prediction
 
 a machine learning application for predicting the likelihood of coral growing given subsea oceanographic conditions.
+
+
+##Usage:
+run the MAIN.py file with the arguments [latitude, longitude, depth, temperature, salinity, dissolved Oxygen]
+
+If data is unknown, enter "None" for the parameter and the program will perform a probabilistic prediction by sweeping through a range of possible values for the unknown variable.
+
--- ml_code/MAIN.py
+++ ml_code/MAIN.py
@@ -2,12 +2,25 @@ import os
 import pathlib
 import CoralClass
 
-modelpath = str(pathlib.Path(os.getcwd()).parent) + '/models/trial0.3.h5'
 
-CoralPredictor = CoralClass.CoralPrediction()
-CoralPredictor.set_model(modelpath)
-conditions = [38, -150, 4000, None, 20, None]
-mean_pred, med_pred = CoralPredictor.predict(conditions)
-print('\n----------------------------------------')
-print('The average likelihood of coral growth is: ', mean_pred*100, '%')
-print('The median likelihood of coral growth is: ', med_pred*100, '%')
+def __main__(modelpath, conditions):
+    CoralPredictor = CoralClass.CoralPrediction()
+    CoralPredictor.set_model(str(pathlib.Path(os.getcwd())) + modelpath)
+    mean_pred, med_pred = CoralPredictor.predict(conditions)
+    print('\n----------------------------------------')
+    print('The average likelihood of coral growth is: ', mean_pred*100, '%')
+    print('The median likelihood of coral growth is: ', med_pred*100, '%')
+
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[2:]
+    modelpath = sys.argv[1]
+    pythonname = sys.argv[0]
+    __main__(modelpath, args)
+
+# modelpath = str(pathlib.Path(os.getcwd()).parent) + '/models/trial0.3.h5'
+#
+#
+# conditions = [38, -150, 4000, None, 20, None]
+
