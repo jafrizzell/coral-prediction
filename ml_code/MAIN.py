23770605bfd33734e6acf3e7586520038bcd7cbf
[Nate Baker] <[bakernate@tamu.edu]>
2022-03-24 15:06:00 -0500

    Creation of jupyter notebook

--- ml_code/MAIN.py
+++ ml_code/MAIN.py
@@ -5,22 +5,25 @@ import CoralClass
 
 def __main__(modelpath, conditions):
     CoralPredictor = CoralClass.CoralPrediction()
-    CoralPredictor.set_model(str(pathlib.Path(os.getcwd())) + modelpath)
+    CoralPredictor.set_model(modelpath)
     mean_pred, med_pred = CoralPredictor.predict(conditions)
     print('\n----------------------------------------')
     print('The average likelihood of coral growth is: ', mean_pred*100, '%')
     print('The median likelihood of coral growth is: ', med_pred*100, '%')
 
 
-if __name__ == '__main__':
-    import sys
-    args = sys.argv[2:]
-    modelpath = sys.argv[1]
-    pythonname = sys.argv[0]
-    __main__(modelpath, args)
+# if __name__ == '__main__':
+#     import sys
+#     args = sys.argv[2:]
+#     modelpath = sys.argv[1]
+#     pythonname = sys.argv[0]
+#     __main__(modelpath, args)
 
-# modelpath = str(pathlib.Path(os.getcwd()).parent) + '/models/trial0.3.h5'
+modelpath = str(pathlib.Path(os.getcwd()).parent) + '/models/trial0.3.h5'
 #
 #
-# conditions = [38, -150, 4000, None, 20, None]
+conditions = [38, -150, 4000, None, 20, None]
+
+__main__(modelpath, conditions)
+
 
